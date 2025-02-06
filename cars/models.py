3665de1cca4948180ada391297c257091641c876
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2025-02-05 23:15:26 -0300

    - Criação dos campos 'plate' e 'photo' no banco de dados

--- cars/models.py
+++ cars/models.py
@@ -17,7 +17,9 @@ class Car(models.Model): #tabela Car herda da classe Models
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand') #campo de chave estrangeira, protegido, relacionado com a tabela Brand
     factory_year = models.IntegerField(blank=True, null=True) #campo de numero inteiro
     model_year = models.IntegerField(blank=True, null=True) #campo de numero inteiro
+    plate = models.CharField(max_length=10, blank=True, null=True) #campo de texto com maximo de caracteres, vazio
     value = models.FloatField(blank=True, null=True) #campo de numero flutuante
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True) #campo de imagem, com upload para a pasta cars, vazio
 
     def __str__(self): #função para retornar o modelo do carro
         return self.model #retorna o modelo do carro
