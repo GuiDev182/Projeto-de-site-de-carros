e9dede995490af33c24e2224a2d844f142c28694
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2025-02-11 23:58:53 -0300

    - modificação da view cars_view retornando os carros, mode e fabricação do banco de dados

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render #importa a função render
+from cars.models import Car
 
 # Create your views here.
-def cars_view(request):
-    return HttpResponse('Hello, World!')
+def cars_view(request): #cria a função cars_view
+    cars = Car.objects.all() #busca todos os carros no banco de dados
+    return render(request,
+                  'cars.html',
+                  {'cars': cars}
+                  ) #retorna a página cars.html com a lista de carros
